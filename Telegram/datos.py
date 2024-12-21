0baf05fcf20c4bd13b35806f5358197791453327
isara5594 <i54841318@gmail.com>
2024-12-21 04:45:49 -1200

    Update datos.py

--- Telegram/datos.py
+++ Telegram/datos.py
@@ -1,7 +1,7 @@
 # Datos de Configuración 
 
 configuracion = {
-    "usuario" : "@MrBitcoinPeru", # Nombre de Usuario (Sólo se usa para guardar la sesión)
+    "usuario" : "@Spamer381", # Nombre de Usuario (Sólo se usa para guardar la sesión)
     "api_id" : 28563593, # API_ID generado desde my.telegram.org 
     "api_hash" : '5223a4f919f2b675128d3b7d71527b5b', # API_HASH generado desde my.telegram.org
     "phone":'+51995137773' # Teléfono (Debe incluir el prefijo)
